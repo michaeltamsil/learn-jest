8bbcd767d6a5a95cd9012489a605b722ef98b510
michael tamsil <michaeltamsil@gmail.com>
2018-09-14 00:37:31 +0700

    lesson: testing asynchronous code - .resolve / .rejects async/await

--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,5 @@
-let axios = require('axios')
+const axios = require('axios')
+const parseJson = require('parse-json')
 const sum = require('./sum')
 
 test('adds 1 + 2 to equal 3', () => {
@@ -39,10 +40,49 @@ test('the data star wars is people 2 named of C-3P0', () => {
     })
 })
 
-test('the data star wars is people 762 is error', () => {
+test('the data star wars is people 762 expect error to be defined', () => {
     expect.assertions(1);
     return axios.get('https://swapi.co/api/people/762/')
     .catch(function (error) {
         expect(error).toBeDefined()
     })
+})
+
+Async/Await
+let name = 'Yavin IV'
+test('the data star wars planet id 3 is ' + name, async () => {
+    expect.assertions(1);
+    const data = await axios.get('https://swapi.co/api/planets/3/').then( response => response.data);
+    expect(data.name).toBe(name)
+})
+
+test('the data star wars planet id 398 status is 404 ', async () => {
+    expect.assertions(1);
+    try {
+        await axios.get('https://swapi.co/api/planets/398/')
+    } catch (e) {
+        expect(e.response.status).toBe(404)
+    }
+})
+
+
+test('the data star wars planet id 3 is ' + name + ' using async await .resolves', async () => {
+    expect.assertions(1);
+    await expect(
+            axios.get('https://swapi.co/api/planets/3/').then(response => response.data.name )
+        )
+        .resolves.toBe(name)
+})
+
+// this is different ... don't know what the best short answer, it's feels weird if fails use resolves
+test('the data star wars planet id 398 status is 404, using async await .rejects', async () => {
+    expect.assertions(1);
+    await expect(
+        axios.get('https://swapi.co/api/planets/398/')
+        .catch(e => {
+            return e.response.status
+        })
+    )
+    //.rejects.toBe(404)
+    .resolves.toBe(404)
 })
